3424918f16c91e15fbe2b7bdbcf6f228237e46ee
Code Annihilator <frostberryart@gmail.com>
2017-08-17 00:33:14 +0300

    adding async actions and mock api

--- src/components/course/CoursesPage.js
+++ src/components/course/CoursesPage.js
@@ -6,21 +6,6 @@ import * as courseActions from '../../actions/courseActions'
 class CoursesPage extends Component {
   constructor(props, context) {
     super(props, context)
-    this.state = {
-      course: { title: '' }
-    }
-    this.onTitleChange = this.onTitleChange.bind(this)
-    this.onClickSave = this.onClickSave.bind(this)
-  }
-
-  onTitleChange(e) {
-    const course = this.state.course
-    course.title = e.target.value
-    this.setState({ course: course })
-  }
-
-  onClickSave() {
-    this.props.actions.createCourse(this.state.course)
   }
 
   courseRow(course, index) {
@@ -32,17 +17,6 @@ class CoursesPage extends Component {
       <div>
         <h1>Courses</h1>
         { this.props.courses.map(this.courseRow) }
-        <h2>Add Course</h2>
-        <input
-          type='text'
-          onChange={this.onTitleChange}
-          value={this.state.course.title}
-        />
-        <input
-          type='submit'
-          value='Save'
-          onClick={this.onClickSave}
-        />
       </div>
     )
   }
